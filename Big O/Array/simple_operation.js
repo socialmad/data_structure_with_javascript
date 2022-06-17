dd2cab3e9be5503cb78c0174bfc33cbdb5f6eb1e
yoursocialmd <yoursocialmd@gmail.com>
2022-06-17 12:03:03 +0530

    simple operation on array

---
+++ Big O/Array/simple_operation.js
@@ -0,0 +1,16 @@
+const string = ['a','b','c','d'];
+
+//push
+string.push('e'); // O(1)
+
+//pop
+string.pop(); //O(1)
+
+//unshift
+string.unshift('x'); // O(n)
+
+//splice
+string.splice(2,0,'Ram'); //O(n)
+
+console.log(string);
+
